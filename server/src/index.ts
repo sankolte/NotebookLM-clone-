3c3661412c839536eeb25ebd151a650a9e2de2cf
Sanskar Kolte <koltesanskar.1406@gmail.com>
2026-08-07 22:45:35 +0530

    auth and user impmeneted

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8081;
 
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
+
+
+app.all("/api/auth/*", toNodeHandler(auth));
+
 
 app.get("/", (req, res) => {
   res.send("server is running");
