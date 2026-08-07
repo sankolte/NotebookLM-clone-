fd759f9b6fe844dc4fde2d17c4088e47d8392ce4
Sanskar Kolte <koltesanskar.1406@gmail.com>
2026-08-07 17:28:23 +0530

    prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,13 +6,16 @@ const app = express();
 const port = process.env.PORT || 8081;
 
 
- app.get("/",(req,res)=>{
-   res.send("server is running");
- }
- )
- app.get("/health",(req,res)=>{
-  res.json({success:true,message:"server is running"});
- })
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ success: true, message: "server is running" });
+});
+
 
  app.listen(port, () => {
    console.log(`server is running on port no ${port}`);
